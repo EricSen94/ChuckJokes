3337a8e76d508a3274f4bf791422a2b8db4a233f
Eric Senen Mora Lopez <eric.sml117@gmail.com>
2021-08-23 13:54:13 -0500

    Changes on css

--- chuckjokes/src/components/typeJoke/typeJoke.jsx
+++ chuckjokes/src/components/typeJoke/typeJoke.jsx
@@ -1,17 +1,8 @@
-import React, { useState, Fragment, useEffect } from 'react';
-import { Dropdown } from 'semantic-ui-react'
+import React, { Fragment } from 'react';
 import { TypeJokeList } from '../TypeJokeList/TypeJokeList'
 import './TypeJoke.css'
-import axios from 'axios';
 
 export function TypeJoke({categories, onButtonClick}) {
-    
-
-    
-
-      const getJoke = (cat) =>{
-        console.log(cat)
-      }
     return (
         <Fragment>
             <div>
